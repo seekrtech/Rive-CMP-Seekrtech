65cc5870af2118aa9e95d5ece08414723b4e96c9
Richard Lu <richlu1018@gmail.com>
2025-11-26 17:39:33 +0800

    chore: Bump version to 0.0.6.9_alpha

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -26,7 +26,7 @@ if (localPropertiesFile.exists()) {
 }
 
 group = "com.seekrtech"
-version = "0.0.6.8_alpha"
+version = "0.0.6.9_alpha"
 kotlin {
     androidTarget {
         publishLibraryVariants("release")
